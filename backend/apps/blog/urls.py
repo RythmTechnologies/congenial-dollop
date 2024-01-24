8dbc2b1649b4c593f190c99d1c3bc038c1ed1947
CoderMungan <codermungan@gmail.com>
2024-01-24 08:56:57 +0300

    feat: update to class for the get request

--- backend/apps/blog/urls.py
+++ backend/apps/blog/urls.py
@@ -3,5 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('intro', intro, name="blog-index"),
-    path('blogs', get_blog_all, name="blogs"),
+    path('blogs', Get_blog_all.as_view(), name="blogs"),
 ]
