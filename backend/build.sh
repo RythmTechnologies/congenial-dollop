287d7485bd986a793b97fcdf7a620267c0fddb3b
enescomruk72 <enescomruk10@gmail.com>
2024-02-03 02:18:47 +0300

    feat: update req.txt and build.sh

--- backend/build.sh
+++ backend/build.sh
@@ -2,7 +2,7 @@
 
 # Build the project
 echo "Building the project..."
-python3.9 -m pip install -r requirements.txt
+python3.9 -m pip install --no-cache-dir -r requirements.txt
 
 echo "Make Migration..."
 python3.9 manage.py makemigrations --noinput
