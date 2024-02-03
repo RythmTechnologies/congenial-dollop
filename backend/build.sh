714b8f3aebc77bb6195ef6af7281cfe086a4fb0d
enescomruk72 <enescomruk10@gmail.com>
2024-02-03 04:03:13 +0300

    feat: django 5.0 update, python v3.12

--- backend/build.sh
+++ backend/build.sh
@@ -2,16 +2,16 @@
 
 
 # Create environment
-python3.9 -m venv venv
+python3.12 -m venv venv
 source venv/bin/activate 
 
 # Build the project
 echo "Building the project..."
-python3.9 -m pip install --upgrade pip
-python3.9 -m pip install --no-cache-dir -r requirements.txt
-python3.9 -m pip install psycopg2-binary --no-cache-dir
+python3.12 -m pip install --upgrade pip
+python3.12 -m pip install --no-cache-dir -r requirements.txt
+python3.12 -m pip install psycopg2-binary --no-cache-dir
 
 echo "Make Migration..."
-python3.9 manage.py makemigrations --noinput
-python3.9 manage.py migrate --noinput
-python3.9 manage.py collectstatic --no-input --clear
+python3.12 manage.py makemigrations --noinput
+python3.12 manage.py migrate --noinput
+python3.12 manage.py collectstatic --no-input --clear
