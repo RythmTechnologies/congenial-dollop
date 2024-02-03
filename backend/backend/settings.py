121e0abda3f198e745a0e9333a276bdd55f2450e
enescomruk72 <enescomruk10@gmail.com>
2024-02-03 03:31:47 +0300

    feat: static !

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -159,9 +159,8 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = 'static/'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 
 
 
