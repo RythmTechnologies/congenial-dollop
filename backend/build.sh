8f570bcc19b67ed4dd12719883b3697aa9240944
enescomruk72 <enescomruk10@gmail.com>
2024-02-03 04:04:38 +0300

    feat: vercel.json runtime python v3.9 to v.3.12

--- backend/build.sh
+++ backend/build.sh
@@ -2,16 +2,16 @@
 
 
 # Create environment
-python3.12 -m venv venv
+python3.10 -m venv venv
 source venv/bin/activate 
 
 # Build the project
 echo "Building the project..."
-python3.12 -m pip install --upgrade pip
-python3.12 -m pip install --no-cache-dir -r requirements.txt
-python3.12 -m pip install psycopg2-binary --no-cache-dir
+python3.10 -m pip install --upgrade pip
+python3.10 -m pip install --no-cache-dir -r requirements.txt
+python3.10 -m pip install psycopg2-binary --no-cache-dir
 
 echo "Make Migration..."
-python3.12 manage.py makemigrations --noinput
-python3.12 manage.py migrate --noinput
-python3.12 manage.py collectstatic --no-input --clear
+python3.10 manage.py makemigrations --noinput
+python3.10 manage.py migrate --noinput
+python3.10 manage.py collectstatic --no-input --clear
