b2a3cfcb6745c91a2a8c1b55c775790968d7a28c
CoderMungan <codermungan@gmail.com>
2024-02-03 16:35:22 +0300

    feat: vercel configurations

--- backend/apps/projects/models.py
+++ backend/apps/projects/models.py
@@ -1,22 +1,26 @@
+from autoslug import AutoSlugField
 from django.db import models
 from tinymce.models import HTMLField
-from autoslug import AutoSlugField
+
 from apps.main.mixin import TimeBasedStampModel
 
 
-#Project Orm Start
+# Project Orm Start
 class Project(TimeBasedStampModel):
     name = models.CharField(("Proje Adı"), max_length=50)
     description = HTMLField(("Proje Açıklaması"))
     image = models.ImageField(("Proje Resmi"), upload_to="projects")
-    source = models.URLField(("Proje Url"), max_length=200, blank = True)
-    slug = AutoSlugField(populate_from="name", editable=False, always_update=True, blank=True)
+    source = models.URLField(("Proje Url"), max_length=200, blank=True)
+    slug = AutoSlugField(
+        populate_from="name", editable=False, always_update=True, blank=True
+    )
 
     def __str__(self) -> str:
         return self.name
-    
-    class Meta:
-        verbose_name = 'Proje'
-        verbose_name_plural = 'Projeler'
-#Project Orm End
 
+    class Meta:
+        verbose_name = "Proje"
+        verbose_name_plural = "Projeler"
+
+
+# Project Orm End
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [".vercel.app"]
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+
     'drf_yasg',
     'corsheaders',
     'rest_framework',
