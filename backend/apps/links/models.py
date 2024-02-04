427d8c4e899b56d2e1e88cc3604cf42c708c0545
CoderMungan <codermungan@gmail.com>
2024-02-04 18:27:30 +0300

    feat: update S3 storage

--- backend/apps/links/models.py
+++ backend/apps/links/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from apps.main.mixin import TimeBasedStampModel
 from apps.sublinks.models import SubLinks
-from autoslug import AutoSlugField
 import uuid
 
 # Links ORM Start
@@ -11,10 +10,10 @@ class Links(TimeBasedStampModel):
   href = models.CharField(("Link Yolu"), max_length=50, blank = True)
   sub_links = models.ManyToManyField(SubLinks, verbose_name=("Sub Link"), blank=True)
   has_subs_links = models.BooleanField(("Subs Linki Var mı?"), default = False)
-  
+
   def __str__(self) -> str:
     return self.name
-  
+
   class Meta:
         verbose_name = 'Link'
         verbose_name_plural = 'Linkler'
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -147,12 +147,28 @@ USE_I18N = True
 
 USE_TZ = True
 
+USE_S3 = 'TRUE'
+
+if USE_S3:
+    # aws settings
+    AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+    AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+    AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME")
+    AWS_DEFAULT_ACL = None
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+    # s3 public media settings
+    PUBLIC_MEDIA_LOCATION = 'media'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3Boto3Storage'
+else:
+    MEDIA_URL = '/media/'
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
 STATIC_URL = "static/"
 STATICFILES_DIRS = os.path.join(BASE_DIR, "static"),
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build","static")
-
-MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Default primary key field type
