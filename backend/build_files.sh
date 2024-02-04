a4c3701dd6fa034c052971ae6b6682070261552d
CoderMungan <codermungan@gmail.com>
2024-02-04 18:47:22 +0300

    feat: update buildsh

--- backend/build_files.sh
+++ backend/build_files.sh
@@ -1,2 +1,9 @@
 pip install --upgrade pip
 pip install -r requirements.txt
+
+echo "Migrate işlemleri başlatılıyor..."
+
+python manage.py makemigrations
+python manage.py migrate
+
+echo "Tamamlandı"
