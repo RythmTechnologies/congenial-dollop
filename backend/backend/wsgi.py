9c6a65e4e5dc4f1226f7429c1e88a8f3de7a9783
CoderMungan <codermungan@gmail.com>
2024-02-04 09:30:00 +0300

    feat: vercel deployment8

--- backend/backend/wsgi.py
+++ backend/backend/wsgi.py
@@ -13,4 +13,4 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
-app = get_wsgi_application()
+apps = get_wsgi_application()
