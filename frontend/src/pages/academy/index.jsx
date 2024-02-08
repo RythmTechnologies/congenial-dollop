aa588563d07d7bc45f2aefb2e3b30f801622bbde
enescomruk72 <enescomruk10@gmail.com>
2024-02-08 03:07:12 +0300

    feat: add blog and academy page, delete works page!

---
+++ frontend/src/pages/academy/index.jsx
@@ -0,0 +1,14 @@
+import { Heading } from "@/components/ui/heading";
+
+export default function Academy() {
+  return (
+    <>
+      <Heading
+        level="h1"
+        className="mb-4 px-4 !text-xl bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 inline-block text-transparent bg-clip-text"
+      >
+        Rythm Akademi pek yakında...
+      </Heading>
+    </>
+  );
+}
--- frontend/src/pages/blog/index.jsx
+++ frontend/src/pages/blog/index.jsx
@@ -1,3 +1,14 @@
+import { Heading } from "@/components/ui/heading";
+
 export default function Blog() {
-  return <div>blog</div>;
+  return (
+    <>
+      <Heading
+        level="h1"
+        className="mb-4 px-4 !text-xl bg-gradient-to-r from-red-600 via-purple-500 to-indigo-400 inline-block text-transparent bg-clip-text"
+      >
+        Rythm Blog pek yakında...
+      </Heading>
+    </>
+  );
 }
