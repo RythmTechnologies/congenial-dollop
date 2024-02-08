986cecc36d03c444c9cdf9c065655a8996b885a0
enescomruk72 <enescomruk10@gmail.com>
2024-02-08 03:04:07 +0300

    feat: add Navbar.Desktop, Navbar.Mobile for responsive header

---
+++ frontend/src/layouts/web/components/header/components/Navbar.Desktop.jsx
@@ -0,0 +1,107 @@
+import React, { useMemo } from "react";
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+  NavigationMenuContent,
+} from "@/components/ui/navigation-menu";
+
+import { Skeleton } from "@/components/ui/skeleton";
+import { BrandLogo } from "@/components/icons";
+import { NavLink } from "react-router-dom";
+import { ListItem } from "./ListItem";
+
+import { useNavbarContext } from "./Navbar";
+
+export function NavbarDesktop() {
+  const { data, isLoading } = useNavbarContext();
+
+  const classicLinks = useMemo(
+    () => data?.filter((navlink) => !navlink.has_subs_links),
+    [data]
+  );
+  let dropDownLinks = useMemo(
+    () => data?.filter((navlink) => navlink.has_subs_links),
+    [data]
+  );
+
+  function ClassicNavLinks({ classicLinks }) {
+    return classicLinks.map((link) => (
+      <NavigationMenuItem key={link.name}>
+        <NavLink
+          to={link.href}
+          className={navigationMenuTriggerStyle()}
+          data-radix-collection-item
+        >
+          {link.name}
+        </NavLink>
+      </NavigationMenuItem>
+    ));
+  }
+
+  function DropDownLinks({ dropDownLinks }) {
+    function ProjectsLinksMain() {
+      return (
+        <li className="row-span-3">
+          <NavigationMenuLink asChild>
+            <a
+              className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md focus:border focus:border-foreground"
+              href="/projeler"
+            >
+              <BrandLogo className="w-full h-40 aspect-auto object-contain" />
+              <div className="mb-2 mt-4 text-lg font-medium">Tüm projeler</div>
+              <p className="text-sm leading-tight text-muted-foreground">
+                Tüm projelere göz atarak yetkinliğimizi görün
+              </p>
+            </a>
+          </NavigationMenuLink>
+        </li>
+      );
+    }
+    return (
+      <>
+        {dropDownLinks.map((navlink) => (
+          <NavigationMenuItem key={navlink.id}>
+            <NavigationMenuTrigger>{navlink.name}</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[1fr_1fr]">
+                {/** Eger proje sayfası ise ekstra render et */}
+                {navlink.href === "/projeler" && <ProjectsLinksMain />}
+                {navlink.sub_links.map((link) => (
+                  <ListItem
+                    key={link.id}
+                    title={link.title}
+                    href={`${navlink.href}${link.href}`}
+                    className="focus:border focus:border-foreground"
+                  >
+                    {link.description}
+                  </ListItem>
+                ))}
+              </ul>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+        ))}
+      </>
+    );
+  }
+
+  return (
+    <NavigationMenu className="container hidden lg:flex max-w-3xl mx-auto bg-transparent text-sm">
+      <NavigationMenuList>
+        {isLoading ? (
+          <li className="row-span-3 h-10 w-96 py-1">
+            <Skeleton className="h-full w-full" />
+          </li>
+        ) : (
+          <>
+            <DropDownLinks dropDownLinks={dropDownLinks} />
+            <ClassicNavLinks classicLinks={classicLinks} />
+          </>
+        )}
+      </NavigationMenuList>
+    </NavigationMenu>
+  );
+}
--- frontend/src/layouts/web/components/header/index.jsx
+++ frontend/src/layouts/web/components/header/index.jsx
@@ -1,17 +1,97 @@
-import { HeaderLogo } from "./components/HeaderLogo";
-import Navbar from "./components/Navbar";
+import { useEffect, useState } from "react";
+import { Link, NavLink, useLocation } from "react-router-dom";
+
+import { CgMenuRightAlt } from "react-icons/cg";
+import { Button } from "@/components/ui/button";
 import { ModeToggle } from "@/components/ui/mode-toggle";
+import { TechLogoComponent } from "@/components/icons";
+import useBreakpoints from "@/hooks/use-breakpoint";
+
+import Navbar from "./components/Navbar";
+import { HeaderLogo } from "./components/HeaderLogo";
+
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 
 export default function Header() {
+  const [open, setOpen] = useState(false);
+  const location = useLocation();
+  const { breakpoint } = useBreakpoints("mobile");
+
+  useEffect(() => {
+    setOpen(false);
+  }, [location]);
+
   return (
-    <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background">
-      <div className="container flex h-14 max-w-4xl items-center">
+    <header className="sticky top-0 z-50 w-full border-b border-border/40  bg-background">
+      <div className="container flex h-14 max-w-6xl items-center">
         <div className="mr-4 flex">
           <HeaderLogo />
-          <Navbar />
+          <Navbar>
+            <Navbar.Desktop />
+          </Navbar>
         </div>
-        <div className="flex flex-1 items-center space-x-2 justify-end">
+        <div className="flex flex-1 items-center space-x-2 justify-end ">
+          {/* <Button variant="link" size="icon" className="p-0 ">
+            <Link
+              to="https://www.linkedin.com/company/rythmtechnologies/"
+              target="_blank"
+              title="Rythm Technologies Linkedin"
+              className="w-full h-full flex items-center justify-center bg-transparent"
+            >
+              <TechLogoComponent
+                logoName="Linkedin"
+                color="bg-primary"
+                size={18}
+              />
+            </Link>
+          </Button>
+          <Button variant="link" size="icon" className="p-0 ">
+            <Link
+              to="https://github.com/RythmTechnologies"
+              target="_blank"
+              title="Rythm Technologies GitHub"
+              className="w-full h-full flex items-center justify-center bg-transparent"
+            >
+              <TechLogoComponent
+                logoName="GitHub"
+                color="bg-primary"
+                size={18}
+              />
+            </Link>
+          </Button> */}
           <ModeToggle />
+          {breakpoint == "mobile" && (
+            <Sheet open={open} onOpenChange={() => setOpen(!open)}>
+              <SheetTrigger asChild>
+                <Button variant="link" size="icon" className="text-foreground">
+                  <CgMenuRightAlt size={24} className="absolute" />
+                  <span className="sr-only">Toggle Menur</span>
+                </Button>
+              </SheetTrigger>
+              <SheetContent className="w-full xs:max-w-sm">
+                <SheetHeader>
+                  <SheetTitle className="text-left flex items-center">
+                    <NavLink>
+                      <span className="font-bold">Rythm Technologies</span>
+                    </NavLink>
+                  </SheetTitle>
+                </SheetHeader>
+                <div className="relative overflow-hidden my-4 h-[calc(100vh-8rem)] pb-10 pl-6">
+                  <div className="h-full w-full rounded-[inherit] overflow-y-auto">
+                    <Navbar>
+                      <Navbar.Mobile />
+                    </Navbar>
+                  </div>
+                </div>
+              </SheetContent>
+            </Sheet>
+          )}
         </div>
       </div>
     </header>
