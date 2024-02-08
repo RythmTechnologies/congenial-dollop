80e1d7259419d6ccb4ab30d9101b88ad0125e7bc
enescomruk72 <enescomruk10@gmail.com>
2024-02-08 03:07:44 +0300

    feat: some issue

--- frontend/src/pages/home/index.jsx
+++ frontend/src/pages/home/index.jsx
@@ -4,12 +4,10 @@ import TestimonialsMarquee from "@/components/TestimonialsMarquee";
 
 export default function Home() {
   return (
-    <main>
+    <>
       <AboutSection />
       <ProjectsSection />
-      <section className="py-4 border-y border-border">
-        <TestimonialsMarquee />
-      </section>
-    </main>
+      <TestimonialsMarquee />
+    </>
   );
 }
