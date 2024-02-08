893a68306789997027a60e8c8826ecd79d6e7189
enescomruk72 <enescomruk10@gmail.com>
2024-02-08 03:02:04 +0300

    feat: add a lot of components.

--- frontend/src/components/icons/logo.jsx
+++ frontend/src/components/icons/logo.jsx
@@ -1,5 +1,8 @@
 import { cn } from "@/lib/utils";
 import logo from "./logo.png";
-export const Logo = ({ className }) => {
-  return <img src={logo} alt="Rythm Technologies Logo" className={className} />;
+
+export const BrandLogo = ({ className }) => {
+  return (
+    <img src={logo} alt="Rythm Technologies Logo" className={cn(className)} />
+  );
 };
